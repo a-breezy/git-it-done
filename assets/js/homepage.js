c01debced593c87dba43c5dd71d59cd4c932bb4e
a.breezy <ambrose.wilkins@gmail.com>
2021-11-18 11:34:03 -0500

    search topic by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 // functions
 
 var displayRepos = function(repos, searchTerm) {
@@ -91,4 +91,33 @@ var getUserRepos = function(user) {
         });
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl =
+        "https://api.github.com/search/repositories?q=" +
+        language +
+        "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear out old content:
+        repoContainerEl.textContent = "";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
